c71333057948c04b9c1dd863a28854c5a1c505c2
SilentChaos <avanishm471@gmail.com>
2023-01-26 19:23:56 +0530

    not so random

---
+++ index.js
@@ -0,0 +1,31 @@
+let firstCard=10
+let secondCard=11
+let sum=firstCard+secondCard
+let HasBlackJack=false
+let suml=document.querySelector("#sum-el")
+let messageEl=document.getElementById("message-el")
+
+isAlive=true
+let message=""
+message="logging out"
+
+
+function StartGame(){
+    suml.textContent= "Sum: " + sum
+
+if(sum<=20){
+    message="do you want to draw a new card"
+
+}
+else if(sum===21){
+message="you have won the blackjack congratulations"
+    HasBlackJack=true
+}
+else{
+    
+    message="oop you suck"
+    isAlive=false
+}
+
+messageEl.textContent=message
+}
