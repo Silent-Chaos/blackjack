b9e3e8c40ade04256627380ce0c51f5bd83b9d8d
SilentChaos <avanishm471@gmail.com>
2023-02-03 17:01:55 +0530

    ?

--- index.js
+++ index.js
@@ -15,65 +15,65 @@ let message=""
 
 let playerEl=document.getElementById("player-el")
 message="logging out"
-playerEl.textContent=player.Name+" : $"+ player.Chip
 
 function getRandomCard(){
-let ranNum = Math.floor(Math.random()*11+1)
-if (ranNum===1){
-return 11
-}
-else if(ranNum>=11){
-return 10
-}
-else{
-    return ranNum
-}
-
+    let ranNum = Math.floor(Math.random()*11+1)
+    if (ranNum===1){
+        return 11
+    }
+    else if(ranNum>=11){
+        return 10
+    }
+    else{
+        return ranNum
+    }
+    
 }
 function StartGame(){
     isAlive=true
     let firstCard=getRandomCard()
-let secondCard=getRandomCard()
-sum=firstCard+secondCard
-cards=[firstCard,secondCard]
-
+    let secondCard=getRandomCard()
+    sum=firstCard+secondCard
+    cards=[firstCard,secondCard]
+    
     renderGame()
 }
 
 function renderGame(){
     cardsEl.textContent= "Your Cards : "
- for(let i=0; i < cards.length; i++){
+    for(let i=0; i < cards.length; i++){
         cardsEl.textContent+=cards[i]+" "
-
+        
     }
     suml.textContent= "Sum: " + sum
-
-if(sum<=20){
-    message="do you want to draw a new card"
-
-}
-else if(sum===21){
-message="you have won the blackjack congratulations"
-    HasBlackJack=true
-}
-else{
     
-    message="oop you suck"
-    isAlive=false
-}
+    if(sum<=20){
+        message="do you want to draw a new card"
+        
+    }
+    else if(sum===21){
+        message="you have won the blackjack congratulations"
+        HasBlackJack=true
+    }
+    else{
+        
+        message="oop you suck"
+        isAlive=false
+    }
+    
 
-
-messageEl.textContent=message
-
+    messageEl.textContent=message
+    
 }
 function newCard(){
-    if(isAlive===true && HasBlackJack===true)
+    if(isAlive===true && HasBlackJack===false)
     {
-let cardd=getRandomCard()
-sum=sum+cardd
-cards.push(cardd)
-renderGame()
-
+        let cardd=getRandomCard()
+        sum=sum+cardd
+        cards.push(cardd)
+        renderGame()
+        
+    }
 }
-}
 
+playerEl.textContent=player.Name+" : $"+ player.Chip
